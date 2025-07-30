eb196cd88c60911e6da62f62ceceb53651a0ba7a
Fazlı Altun <altunfazl@gmail.com>
2025-07-30 11:24:40 +0300

    models are created

---
+++ main.py
@@ -0,0 +1,8 @@
+from utils import save_video, read_video
+
+def main():
+    video_frames = read_video("input_videos/video_1.mp4")
+
+    save_video = (video_frames, "output_videos/output_video_1.avi")
+if __name__ == "__main__":
+    main()
