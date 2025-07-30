568bc24a07ec7ce98390558cd87954cb1ebe0ca0
Fazlı Altun <altunfazl@gmail.com>
2025-07-30 15:51:18 +0300

    player tracks drawer is added

---
+++ drawers/player_tracks_drawer.py
@@ -0,0 +1,17 @@
+class PlayerTracksDrawer:
+
+    def __init__(self):
+        pass
+
+    def draw(self, video_frames, tracks):
+        output_video_frames = []
+        for frame_num, frame in enumerate(video_frames):
+            frame = frame.copy()
+
+            player_dict = tracks[frame_num]
+
+            # draw player tracks
+            for track_id, player in player_dict.items():
+                frame = draw_ellipse(frame, player["box"], (0,0,255))
+                output_video_frames.append(frame)
+        return output_video_frames
